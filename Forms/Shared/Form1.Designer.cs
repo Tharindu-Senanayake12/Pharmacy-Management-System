1ed45381b906b10cbc9cd8003d69589822b7f573
Tharindu Senanayake <tharindusenanayake134@gmail.com>
2025-12-13 01:49:31 +0530

    Form1 designer initial

---
+++ Forms/Shared/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace PharmacyManagementSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.close = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.login_username = new Guna.UI2.WinForms.Guna2TextBox();
+            this.login_password = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.login_ShowPass = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.loginBtn = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.login_registerBtn = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 500);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pictureBox1.Image = global::PharmacyManagementSystem.Properties.Resources.pharmacyIcon;
+            this.pictureBox1.Location = new System.Drawing.Point(178, 170);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 41);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Inter", 12F);
+            this.label8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label8.Location = new System.Drawing.Point(64, 266);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(272, 19);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "- Pharmacy Management System -";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(136, 226);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(129, 32);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "MediTrust";
+            // 
+            // close
+            // 
+            this.close.AutoSize = true;
+            this.close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.close.Font = new System.Drawing.Font("Segoe Fluent Icons", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close.Location = new System.Drawing.Point(778, 1);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(20, 21);
+            this.close.TabIndex = 1;
+            this.close.Text = "X";
+            this.close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Inter", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(446, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Inter", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(446, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // login_username
+            // 
+            this.login_username.BorderRadius = 9;
+            this.login_username.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.login_username.DefaultText = "";
+            this.login_username.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.login_username.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.login_username.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.login_username.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.login_username.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.login_username.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.login_username.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.login_username.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.login_username.Location = new System.Drawing.Point(450, 183);
+            this.login_username.Margin = new System.Windows.Forms.Padding(4);
+            this.login_username.Name = "login_username";
+            this.login_username.PasswordChar = '\0';
+            this.login_username.PlaceholderText = "";
+            this.login_username.SelectedText = "";
+            this.login_username.Size = new System.Drawing.Size(302, 38);
+            this.login_username.TabIndex = 1;
+            this.login_username.TextChanged += new System.EventHandler(this.login_username_TextChanged);
+            // 
+            // login_password
+            // 
+            this.login_password.BorderRadius = 9;
+            this.login_password.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.login_password.DefaultText = "";
+            this.login_password.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.login_password.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.login_password.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.login_password.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.login_password.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.login_password.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.login_password.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.login_password.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.login_password.Location = new System.Drawing.Point(450, 266);
+            this.login_password.Margin = new System.Windows.Forms.Padding(4);
+            this.login_password.Name = "login_password";
+            this.login_password.PasswordChar = '•';
+            this.login_password.PlaceholderText = "";
+            this.login_password.SelectedText = "";
+            this.login_password.Size = new System.Drawing.Size(302, 38);
+            this.login_password.TabIndex = 2;
+            this.login_password.TextChanged += new System.EventHandler(this.login_password_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Satoshi", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(544, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 26);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Hello there!";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Satoshi", 18.5F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(507, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(195, 31);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Welcome Back!";
+            // 
+            // login_ShowPass
+            // 
+            this.login_ShowPass.AutoSize = true;
+            this.login_ShowPass.Font = new System.Drawing.Font("Inter", 10F);
+            this.login_ShowPass.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.login_ShowPass.Location = new System.Drawing.Point(453, 318);
+            this.login_ShowPass.Name = "login_ShowPass";
+            this.login_ShowPass.Size = new System.Drawing.Size(131, 21);
+            this.login_ShowPass.TabIndex = 5;
+            this.login_ShowPass.Text = "Show Password";
+            this.login_ShowPass.UseVisualStyleBackColor = true;
+            this.login_ShowPass.CheckedChanged += new System.EventHandler(this.login_ShowPass_CheckedChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Inter", 10F);
+            this.label6.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label6.Location = new System.Drawing.Point(486, 428);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(162, 17);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Don\'t have an account?";
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.BorderRadius = 9;
+            this.loginBtn.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.loginBtn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.loginBtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.loginBtn.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.loginBtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.loginBtn.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginBtn.ForeColor = System.Drawing.Color.White;
+            this.loginBtn.Location = new System.Drawing.Point(450, 367);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(302, 45);
+            this.loginBtn.TabIndex = 3;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // login_registerBtn
+            // 
+            this.login_registerBtn.AutoSize = true;
+            this.login_registerBtn.Font = new System.Drawing.Font("Inter", 10F);
+            this.login_registerBtn.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.login_registerBtn.LinkColor = System.Drawing.SystemColors.MenuText;
+            this.login_registerBtn.Location = new System.Drawing.Point(650, 428);
+            this.login_registerBtn.Name = "login_registerBtn";
+            this.login_registerBtn.Size = new System.Drawing.Size(62, 17);
+            this.login_registerBtn.TabIndex = 6;
+            this.login_registerBtn.TabStop = true;
+            this.login_registerBtn.Text = "Register";
+            this.login_registerBtn.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.login_registerBtn_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.login_registerBtn);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.login_ShowPass);
+            this.Controls.Add(this.login_password);
+            this.Controls.Add(this.login_username);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label close;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI2.WinForms.Guna2TextBox login_username;
+        private Guna.UI2.WinForms.Guna2TextBox login_password;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox login_ShowPass;
+        private System.Windows.Forms.Label label6;
+        private Guna.UI2.WinForms.Guna2GradientButton loginBtn;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel login_registerBtn;
+    }
+}
+
